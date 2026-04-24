b26e875efaacdc18152997398bf9c5716342d8e3
Jhonathan Dominick <jhonathandominickdemelo@gmail.com>
2026-04-24 14:26:01 -0300

    feat(bff): bootstrap inicial do bff-pilates com endpoint health

---
+++ backend/bff-pilates/src/main/java/com/rockepilates/bff/BffPilatesApplication.java
@@ -0,0 +1,12 @@
+package com.rockepilates.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffPilatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BffPilatesApplication.class, args);
+    }
+}
