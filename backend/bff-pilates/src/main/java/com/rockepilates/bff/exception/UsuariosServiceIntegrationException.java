8811fd491da6e0c3f6c7206633209b34cd07e186
Jhonathan Dominick <jhonathandominickdemelo@gmail.com>
2026-04-26 07:07:40 -0300

    feat(bff): adiciona tratamento de erro na integração com usuarios-service

---
+++ backend/bff-pilates/src/main/java/com/rockepilates/bff/exception/UsuariosServiceIntegrationException.java
@@ -0,0 +1,8 @@
+package com.rockepilates.bff.exception;
+
+public class UsuariosServiceIntegrationException extends RuntimeException {
+
+    public UsuariosServiceIntegrationException(String message) {
+        super(message);
+    }
+}
