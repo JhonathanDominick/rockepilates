40bf5ad0819106b2bda7f668855b49eca3625442
Jhonathan Dominick <jhonathandominickdemelo@gmail.com>
2026-04-27 15:26:39 -0300

    feat(bff): adiciona busca de usuário por id

--- backend/bff-pilates/src/main/java/com/rockepilates/bff/client/UsuariosClient.java
+++ backend/bff-pilates/src/main/java/com/rockepilates/bff/client/UsuariosClient.java
@@ -1,12 +1,14 @@
 package com.rockepilates.bff.client;
 
 import com.rockepilates.bff.dto.PagedResponse;
+import com.rockepilates.bff.dto.SuccessResponse;
 import com.rockepilates.bff.dto.UsuarioResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.rockepilates.bff.config.FeignConfig;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
         name = "usuarios-service",
@@ -21,5 +23,11 @@ public interface UsuariosClient {
             @RequestHeader("Authorization") String authorizationHeader,
             @RequestParam int page,
             @RequestParam int size
+    );
+
+    @GetMapping("/usuarios/{id}")
+    SuccessResponse<UsuarioResponse> buscarUsuarioPorId(
+            @RequestHeader("Authorization") String authorizationHeader,
+            @PathVariable Long id
     );
 }
