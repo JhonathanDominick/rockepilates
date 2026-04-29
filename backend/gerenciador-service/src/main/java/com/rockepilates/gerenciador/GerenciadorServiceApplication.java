d6bb289460fc909ec5b6518537a4202f9616881d
Jhonathan Dominick <jhonathandominickdemelo@gmail.com>
2026-04-29 15:55:52 -0300

    feat(gerenciador-service): cria estrutura inicial do microserviço com actuator e integração com PostgreSQL

---
+++ backend/gerenciador-service/src/main/java/com/rockepilates/gerenciador/GerenciadorServiceApplication.java
@@ -0,0 +1,12 @@
+package com.rockepilates.gerenciador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorServiceApplication.class, args);
+    }
+}
